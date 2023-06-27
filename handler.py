d0247bba3b412362e899c1debc8b5c85c914292b
Ricardoamp <102176416+Ricardoamp@users.noreply.github.com>
2023-06-27 10:51:50 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -38,4 +38,4 @@ def Health_Insurance_Predict():
 
 if __name__ == '__main__':
     port = os.environ.get( 'PORT', 5000 )
-    app.run( host = '0.0.0.0', port = port, debug=True )
+    app.run( host = '127.0.0.1', port = port, debug=True )
